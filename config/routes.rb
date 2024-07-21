f7d2c5f1eb5423488c73566628a89ff6838b2fa8
felipealmeida92 <129707274+felipealmeida92@users.noreply.github.com>
2024-07-21 12:04:22 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  # Rota para verificar a saúde da aplicação
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Rotas RESTful para as traduções
   resources :translations, only: [:new, :create, :show]
 
-  # Define a rota raiz da sua aplicação, se necessário
-  # root "controller_name#action_name"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "translations#new"
 end
 
